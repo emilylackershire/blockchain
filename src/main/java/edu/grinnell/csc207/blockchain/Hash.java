b7f1887297be7d99cda00b8238d943f6b29afa13
Emily <lackersh@grinnell.edu>
2025-03-02 18:24:18 -0600

    implemented hash

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,19 @@
 package edu.grinnell.csc207.blockchain;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
-    // TODO: fill me in!
+    public static byte[] calculateHash(String msg) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+        md.update(msg.getBytes());
+        byte[] hash = md.digest();
+        return hash;
+    }
+
+
+
+
 }
