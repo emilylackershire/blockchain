c140b746ef6d0e60851dbab83be26cfe2e97f682
Emily <lackersh@grinnell.edu>
2025-03-02 22:10:51 -0600

    implemented most of the block class

--- src/main/java/edu/grinnell/csc207/blockchain/Block.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Block.java
@@ -4,5 +4,41 @@ package edu.grinnell.csc207.blockchain;
  * A single block of a blockchain.
  */
 public class Block {
-    // TODO: fill me in!
+    int number = 0;
+    int amount;
+    Hash prevHash;
+    long nonce;
+    Hash hash;
+    public Block(int num, int amount, Hash prevHash) {
+        this.number = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+    }
+    public Block(int num, int amount, Hash prevHash, long nonce) {
+        this.number = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        this.nonce = nonce;
+
+    }
+    public int getNum() {
+        return number;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public long getNonce() {
+        return nonce;
+    }
+    public Hash getPrevHash() {
+        return prevHash;
+    } 
+    public Hash getHash() {
+        return hash;
+    }
+    public String toString() {
+        return "Block " + getNum() + " (Amount: " + getAmount() + ", Nonce: " + getNonce() + ", prevHash: " + getHash() + ", hash: " + getHash() +")";
+    }
+
 }
+
