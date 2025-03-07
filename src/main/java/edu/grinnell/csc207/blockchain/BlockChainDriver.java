5c5d053ff80d087567c2253b94c8196067f884ea
Emily <lackersh@grinnell.edu>
2025-03-07 12:49:36 -0600

    worked on main class

--- src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
+++ src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
@@ -1,5 +1,8 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+
 /**
  * The main driver for the block chain program.
  */
@@ -10,7 +13,46 @@ public class BlockChainDriver {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // TODO: fill me in!
+        if (args.length != 1 ) {
+            System.err.println("Usage: java BlockChain: missing an initial amount");
+            System.exit(1);
+        }
+        int initialAmount = Integer.valueOf(args[0]);
+        if (initialAmount < 0) {
+            System.err.println("Error: cannot be negative");
+        }
+        BlockChain block = new BlockChain(initialAmount);
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Command?");
+        switch(input) {
+            case "mine":
+                break;
+            case "append":
+                break;
+            case "remove":
+                break;
+            case "check":
+                break;
+            case "report":
+                break;
+            case "help":
+                break;
+            case "quit":
+                break;
+            default:
+                System.out.println("Command?");
+        }
+
+
+
+
+
+
+
+
+
+
+        scan.close();
     }  
 }
